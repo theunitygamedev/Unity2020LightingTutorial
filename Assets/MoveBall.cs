6bbeed93dabca224a703d4243ccaa6763735b343
Frederik Koetje <fkoetje@ullico.com>
2020-09-08 22:57:08 -0400

    added all files

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public float moveAmount;
+    Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = this.transform;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.position = new Vector2(t.position.x, t.position.y + moveAmount);
+        if (t.position.y >= 1.5 || t.position.y < 0.168)
+        {
+            moveAmount = -1 * moveAmount;
+        }
+    }
+}
